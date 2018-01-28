c1fd2374a72980c5ee98563f0695744622f6b7ec
AnidaKhizar <anida-khizar@hotmail.fr>
2018-01-28 22:33:56 +0100

    removed cleaned solutions

--- subgradient_test.m
+++ subgradient_test.m
@@ -16,7 +16,7 @@ a_vec = tmp(nvars+1:2*nvars);
 b = tmp(2*nvars+1:end);
 
 % compose a matrix from a vector
-a = vector_to_matrix(m, n, a_vec);
+a = reshape(a_vec, [n,m])';
 
 % define parameters used in sub_grad algorithm
 epsilon = 0.1;
@@ -29,11 +29,8 @@ DualNoChangTOL = 2;
 
 disp('Objective function value:')
 disp(theta_pi_k)
-disp('dirty x:')
+disp('x:')
 disp( reshape(x_k(1:m*n), m,n)' )
-disp('clean x:')
-clean_x = sol_cleanup_ps(m, n, x_k, a_vec, c);
-disp(clean_x)
 
 end
 
@@ -207,61 +204,4 @@ for i = 1:m
     end
 end
 
-end
-
-function x = sol_cleanup_ps(m, n, dirty_x, A_vec, c)
-    % get A and c as matrix
-    A_mat = vector_to_matrix(m, n, A_vec);
-    c_mat = vector_to_matrix(m, n, c);
-    
-    % compute ratio matrix
-    ratios = -c_mat./A_mat;
-    
-    % new x is only zero, except for 1 coeff at the max efficacity pos
-    x = zeros(m, n);
-    % get dirty_x as matrix
-    dirty_x_mat = vector_to_matrix(m, n, dirty_x);
-    % foreach column of the matrix
-    for j = 1:n
-        col = dirty_x_mat(:,j);
-        col = round(col, 4);
-%        disp([col floor(col) (col ~= floor(col))])
-        % check if column has non-integer values
-        if any(col ~= floor(col) > 0)
-            % get matching ratio column
-            r = ratios(:, j);
-            % get first min index
-            ind = find(r == min(r), 1, 'first');
-            % apply 1 coeff to x vector at the pos corresponding to r min
-            % value
-            x(ind, j) = 1;
-        else
-            x(:,j) = dirty_x_mat(:,j);
-        end
-    end
-end
-
-function M = vector_to_matrix(m, n, v)
-    if m == 1
-        if isrow(v)
-            M = v;
-            return;
-        else
-            M = v';
-            return;
-        end
-    elseif n == 1
-        if isrow(v)
-            M = v';
-            return;
-        else
-            M = v;
-            return;
-        end
-    end
-
-    M = zeros(m,n);
-    for i = 1:m
-        M(i,:) = v((i-1)*n+1:(i-1)*n+n);
-    end
 end
